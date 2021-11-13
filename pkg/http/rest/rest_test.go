92fd51617598cf8223fd91aa3165efdb2c2f1c5c
Marco <marco@task.media>
2021-11-14 00:55:52 +0100

    add checkEndpointPostStatuscode to test POST status codes
    this will extend the existing status code check for GET method with POST method.

--- pkg/http/rest/rest_test.go
+++ pkg/http/rest/rest_test.go
@@ -3,6 +3,7 @@ package rest
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -15,6 +16,21 @@ func checkEndpointGetStatuscode(t *testing.T, httpEndpoint string, expectedHttpS
 	w := httptest.NewRecorder()
 
 	req, _ := http.NewRequest(http.MethodGet, httpEndpoint, nil)
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, expectedHttpStatuscode, w.Code)
+}
+
+// checkEndpointPostStatuscode tests for a given response code for POST method at defined endpoint (path) and payload
+// e.g. status code 400 for invalid post content
+func checkEndpointPostStatuscode(t *testing.T, httpEndpoint, httpPayload, httpContentType string, expectedHttpStatuscode int) {
+	r := SetupRouter()
+	w := httptest.NewRecorder()
+
+	body_reader := strings.NewReader(httpPayload)
+
+	req, _ := http.NewRequest(http.MethodPost, httpEndpoint, body_reader)
+	req.Header.Set("Content-Type", httpContentType)
 	r.ServeHTTP(w, req)
 
 	assert.Equal(t, expectedHttpStatuscode, w.Code)
